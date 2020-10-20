0d8d6097c8f76d4f7b187f9c67072566f9197045
first <papawin1st@gmail.com>
2020-10-20 14:14:25 +0700

    he

--- helloworld.java
+++ helloworld.java
@@ -1,25 +1,16 @@
 /**
  * helloworld
  */
-import java.util.Scanner;
+
 public class helloworld {
    public static void main(String[] args) {
-        Scanner sn = new Scanner(System.in);
-
-        String name;  
-        int number;
-
-        System.out.println("\tGreeting program");
-
-        System.out.print("Enter your name: ");
-        name = sn.next();
-    
-
-        System.out.print("Enter your favorite number: ");
-        number = sn.nextInt();
-
-        System.out.print("Hello " + name);
-        System.out.println(", your favorite number is " + number);
+      int[] card = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
+      int j = 0;
+      int[] A,B,C,D={0} ;
+      for(int i = 0; i<=9 ; i++){
+         A[j] = card[i];
+         B[j+1]= card[i+1];
+       }
 
    }
     
